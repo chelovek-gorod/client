2ab3f2e0754e9c8ceef3bd54f849e32523b479dd
chelovek-gorod <chelovek-gorod@yandex.by>
2021-12-27 00:25:14 +0300

    add

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const container = document.getElementById('container');
+
+let myWs = new WebSocket('ws:https://mars-chat-server.herokuapp.com/8282:');
+myWs.onopen = function () {
+  console.log('подключился');
+};
+
+
+/*
+// обработчик сообщений от сервера
+myWs.onmessage = function (message) {
+  console.log('Message: %s', message.data);
+};
+// функция для отправки echo-сообщений на сервер
+function wsSendEcho(value) {
+  myWs.send(JSON.stringify({action: 'ECHO', data: value.toString()}));
+}
+// функция для отправки команды ping на сервер
+function wsSendPing() {
+  myWs.send(JSON.stringify({action: 'PING'}));
+}
+*/
