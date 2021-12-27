8fac1d7bfb9f9cd25f343e8edbe6c4625e06c878
Marsel <47145234+chelovek-gorod@users.noreply.github.com>
2021-12-27 21:16:45 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 const container = document.getElementById('container');
 
-let myWs = new WebSocket('wss:https://mars-chat-server.herokuapp.com:13842');
+let myWs = new WebSocket('wss:https://mars-chat-server.herokuapp.com:55798');
 myWs.onopen = function () {
   console.log('подключился');
 };
