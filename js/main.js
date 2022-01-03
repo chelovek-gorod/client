82032c4863793284d75c3848fb0fcbe6adfd331a
Marsel <chelovek-gorod@yandex.by>
2022-01-03 16:46:17 +0300

    update

--- js/main.js
+++ js/main.js
@@ -2,11 +2,23 @@
 
 // CONNECTION
 
-const socketURL = 'wss://mars-chat-server.herokuapp.com' // 'ws://localhost:9000'
+const socketURL = 'ws://localhost:9000' // 'wss://mars-chat-server.herokuapp.com' 
 const connectionTimeout = 6000;
 
 let connectionIs = false;
 let registrationIs = false;
+
+let author = null;
+
+class Message {
+  constructor(author, target, message, sticker) {
+    this.author = author; // {nickName, avatar}
+    this.target = target; // {nickName, avatar}
+    this.message = message; // string
+    this.sticker = sticker; // data
+    this.date = Date.now();
+  }
+};
 
 function connection() {
   console.log('--connection request--');
@@ -26,12 +38,10 @@ function connection() {
   socket.onmessage = function (message) {
     let { action, data } = JSON.parse(message.data);
     switch (action) {
-      case 'firstConnect' : getConnectionStart(data); break;
-      case 'registration' : getRegistrationResponse(data); break;
+      case 'firstConnect' : getConnectionStart(data, socket); break;
+      case 'registration' : getRegistrationResponse(data, socket); break;
       case 'onConnect' : getOnConnectResponse(data); break;
-      case 'newUser' : getNewUserResponse(data); break;
-      case 'disconnectionUser' : getDisconnectionUserResponse(data); break;
-      case 'newMessage' : getNewMessageResponse(data); break;
+      case 'newMessage' : getNewMessage(data); break;
       default : getWrongActionInResponse(action, data);
     }
   };
@@ -68,13 +78,13 @@ function connection() {
 
 }
 
-function getConnectionStart(usedAvatarsArr) {
+function getConnectionStart(usedAvatarsArr, socket) {
   console.group('--connection start--');
   console.log('used avatars array:');
   console.log(usedAvatarsArr);
   console.groupEnd();
 
-  registration(usedAvatarsArr);
+  registration(usedAvatarsArr, socket);
 }
 
 function getOnConnectResponse(data) {
@@ -82,20 +92,93 @@ function getOnConnectResponse(data) {
   console.log(`--PingPong[client -> server : ${serverSendTime - clientSendTime}ms; server -> client: ${Date.now() - serverSendTime}ms]--`);
 }
 
-function getRegistrationResponse(data) {
-  console.log(`--registration data: ${data}--`);
+function getRegistrationResponse(data, socket) {
+  let { registrationIs, avatarIs, nickNameIs, messages } = data;
+
+  if (!registrationIs) {
+    socket.send(JSON.stringify({ action: 'firstConnect' }));
+    if (!avatarIs) showModalMessage('Аватар уже занят');
+    else if (!nickNameIs) showModalMessage('Ник-нейм уже занят');
+    else showModalMessage('Ошибка регистрации');
+  } else {
+    containerDiv.innerHTML = '';
+
+    messageListDiv = document.createElement("div");
+    messageListDiv.id = 'messages';
+    containerDiv.append(messageListDiv);
+
+    let messageInput = document.createElement("input");
+    messageInput.id = 'messageInput';
+    containerDiv.append(messageInput);
+
+    let sendButton = document.createElement("button");
+    sendButton.id = 'sendButton';
+    sendButton.innerHTML = 'Отправить';
+    sendButton.onclick = function() {
+      let messageText = messageInput.value.trim();
+      if (!messageText) showModalMessage('Вы не ввели сообщение');
+      else {
+        let message = new Message(author, null, messageText, null);
+        messageInput.value = '';
+        socket.send(JSON.stringify({ action: 'newMessage', data: message}));
+      }
+    };
+    containerDiv.append(sendButton);
+  }
 }
 
-function getNewUserResponse(data) {
-  console.log(`--new user: ${data}--`);
-}
+let messageListDiv;
 
-function getDisconnectionUserResponse(data) {
-  console.log(`--user disconnect: ${data}--`);
-}
+function getNewMessage(messageData) {
+  let { author, target, message, sticker, date } = messageData;
 
-function getNewMessageResponse(data) {
-  console.log(`--new message: ${data}--`);
+  let messageDiv = document.createElement("div");
+
+  if(!author) {
+    // SYSTEM MESSAGE
+    messageDiv.className = 'notification';
+
+    let messageImg = document.createElement("img");
+    messageImg.src = avatarImgPath + target.avatar + avatarImgType;
+    messageDiv.append(messageImg);
+
+    let messageTxt = document.createElement("span");
+    let messageSystemText;
+    switch(message) {
+      case 'newConnect' : messageSystemText = 'теперь в чате'; break;
+      case 'disconnect' : messageSystemText = 'покинул чат'; break;
+      default : messageSystemText = '--';
+    }
+    console.log(messageSystemText);
+    messageTxt.innerHTML = `<span class="nickName">${target.nickName}</span> ${messageSystemText}`;
+    messageDiv.append(messageTxt);
+
+  } else {
+    // USER MESSAGE
+    messageDiv.className = 'message';
+    let messageAuthorAvatar = document.createElement("img");
+    messageAuthorAvatar.src = avatarImgPath + author.avatar + avatarImgType;
+    messageAuthorAvatar.className = 'avatar';
+    messageDiv.append(messageAuthorAvatar);
+
+    let messageAuthorNickName = document.createElement("div");
+    messageAuthorNickName.innerText = author.nickName;
+    messageAuthorNickName.className = 'nick-name';
+    messageDiv.append(messageAuthorNickName);
+
+    let messageContent = document.createElement("div");
+    messageContent.innerText = message;
+    messageContent.className = 'message-text';
+    messageDiv.append(messageContent);
+
+    let messageDate = document.createElement("div");
+    messageDate.innerText = date;
+    messageDate.className = 'message-date';
+    messageDiv.append(messageDate);
+
+  }
+
+  messageListDiv.append(messageDiv);
 }
 
 function getWrongActionInResponse(action, data) {
@@ -137,7 +220,13 @@ function connectionShow(status) {
   }
 }
 
-function registration (usedAvatarsArr) {
+function registration (usedAvatarsArr, socket) {
+  containerDiv.innerHTML = '';
+
+  let avatarsImgArr = [];
+  let chosenAvatarNode;
+  let chosenAvatarName;
+
   let titleDiv = document.createElement("div");
   titleDiv.id = 'avatarsTitle';
   titleDiv.innerText = 'Выберете Аватарку';
@@ -148,37 +237,62 @@ function registration (usedAvatarsArr) {
   containerDiv.append(avatarsDiv);
 
   let nickNameInput = document.createElement("input");
-  nickNameInput.id = 'nickName';
+  nickNameInput.id = 'inputNickName';
   containerDiv.append(nickNameInput);
 
   let registrationButton = document.createElement("button");
-  registrationButton.id = 'registration';
+  registrationButton.id = 'registrationButton';
   registrationButton.innerHTML = 'Регистрация';
+  registrationButton.onclick = function() {
+    let nickName = nickNameInput.value.trim();
+    if (!chosenAvatarNode) showModalMessage('Вы не выбрали аватар');
+    else if (!nickName) showModalMessage('Пустой ник-нейм');
+    else if (nickName.length < 2) showModalMessage('Слишком короткий ник-нейм<br>(нужно от 2х до 20ти символов)');
+    else if (nickName.length > 20) showModalMessage('Слишком длинный ник-нейм<br>(нужно от 2х до 20ти символов)');
+    else {
+      console.group('Registration');
+      console.log('chosenAvatar');
+      console.log(chosenAvatarName);
+      console.groupEnd();
+
+      author = {nickName: nickName, avatar: chosenAvatarName};
+      socket.send(JSON.stringify({ action: 'registration', data: author}));
+    }
+  };
   containerDiv.append(registrationButton);
 
-  fillAvatarsDiv(avatarsDiv, nickNameInput, usedAvatarsArr);
-
-}
-
-function fillAvatarsDiv(div, input, disableAvatars) {
-  div.innerHTML = '';
-
-  let avatarsImgArr = [];
-  let chosenAvatar;
-
   avatarsArr.forEach((img, index) => {
-    let disable = ~disableAvatars.indexOf(img);
+    let disable = ~usedAvatarsArr.indexOf(img);
     
     let avatarImg = document.createElement('img');
     avatarImg.src = avatarImgPath + img + avatarImgType;
     if (disable) avatarImg.className = 'disable';
     else avatarImg.onclick = function() {
-      if (chosenAvatar) chosenAvatar.classList.remove('choose');
-      chosenAvatar = this;
+      if (chosenAvatarNode) chosenAvatarNode.classList.remove('choose');
+      chosenAvatarNode = this;
+      chosenAvatarName = img;
       this.classList.add('choose');
-      input.value = avatarsNames[index];
+      nickNameInput.value = avatarsNames[index];
     };
     avatarsImgArr.push(avatarImg);
-    div.append(avatarImg);
+    avatarsDiv.append(avatarImg);
   });
+
+}
+
+function showModalMessage(message) {
+  let modalShell = document.createElement("div");
+  modalShell.id = 'modalShell';
+  modalShell.className = 'full-screen flex-wrapper';
+  modalShell.style.zIndex = 3;
+  modalShell.onclick = function() {
+    this.style.opacity = 0;
+    setTimeout(() => this.remove(), 600);
+  };
+
+  let modalMessage = document.createElement("div");
+  modalMessage.innerHTML = message;
+  modalShell.append(modalMessage);
+
+  document.body.append(modalShell);
 }
